696c4219dabeb16682a61adb255237389597ad0c
JayTrairat <JayTrairat@JayTrairats-MacBook-Pro.local>
2018-04-17 15:03:51 +0700

    feat(): ranked completed

---
+++ ranking.py
@@ -0,0 +1,15 @@
+from operator import itemgetter
+
+def main():
+    with open('assets/first_cosine_values.txt', 'r', encoding='utf8') as source:
+        contents = source.readlines()
+        score_contents = [{'word': content.strip().split('=')[0], 'score': content.strip().split('=')[1]} for content in contents]
+        score_contents = sorted(score_contents, key=itemgetter('score'), reverse=True)
+        score_contents = score_contents[:10]
+        score_contents = [content['word'] + '=' + content['score'] for content in score_contents]
+
+    with open('assets/first_cosine_values_ranked.txt', 'w', encoding='utf8') as result:
+        result.write('\n'.join(score_contents))
+
+if __name__ == '__main__':
+    main()
