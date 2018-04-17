50a6a55430dd567c5ef2616af3f82032d063f535
JayTrairat <JayTrairat@JayTrairats-MacBook-Pro.local>
2018-04-17 15:48:21 +0700

    feat(): stats preparation

---
+++ 4_stats_preparation.py
@@ -0,0 +1,25 @@
+from collections import Counter as count
+
+def main():
+        with open('assets/first_cosine_values_ranked_tagged.txt', 'r', encoding='utf8') as source:
+            contents = (source.readlines())
+            contents = [content.strip().split('=')[0].split('|') for content in contents]
+            contents = [content for content in contents if len(content) == 3]
+
+            first_position = [content[0] for content in contents]
+            second_position = [content[1] for content in contents]
+            third_position = [content[2] for content in contents]
+
+            first_position = [content[1:-1].split(',')[1][2:-1] for content in first_position]
+            second_position = [content[1:-1].split(',')[1][2:-1] for content in second_position]
+            third_position = [content[1:-1].split(',')[1][2:-1] for content in third_position]
+
+        with open('assets/first_cosine_values_ranked_tagged_stats.txt', 'w', encoding='utf8') as result:
+            result.write('first_position :: ' + str(count(first_position)))
+            result.write('\n')
+            result.write('second_position :: ' + str(count(second_position)))
+            result.write('\n')
+            result.write('third_position :: ' + str(count(third_position)))
+
+if __name__ == '__main__':
+    main()
