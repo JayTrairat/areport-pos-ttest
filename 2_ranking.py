f0032ebc024acd746e1e1cba5b371b5a0aff076a
JayTrairat <JayTrairat@JayTrairats-MacBook-Pro.local>
2018-04-17 15:50:04 +0700

    refactor(): pick top 100

--- 2_ranking.py
+++ 2_ranking.py
@@ -5,7 +5,7 @@ def main():
         contents = source.readlines()
         score_contents = [{'word': content.strip().split('=')[0], 'score': content.strip().split('=')[1]} for content in contents]
         score_contents = sorted(score_contents, key=itemgetter('score'), reverse=True)
-        score_contents = score_contents[:30]
+        score_contents = score_contents[:100]
         score_contents = [content['word'] + '=' + content['score'] for content in score_contents]
 
     with open('assets/first_cosine_values_ranked.txt', 'w', encoding='utf8') as result:
